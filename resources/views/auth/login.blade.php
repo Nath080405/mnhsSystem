0315bf48ebece6e5569473188261aeea9a42bb4e
leslieMarie20 <lesliemarie.lumapac.23@usjr.edu.ph>
2025-05-25 09:16:33 +0800

    Update login
    ID number change "ID number"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="mb-3">
-                            <label for="username" class="form-label text-muted small">School ID</label>
+                            <label for="username" class="form-label text-muted small">ID Number</label>
                             <div class="input-group shadow-sm">
                                 <span class="input-group-text bg-white border-end-0">
                                     <i class="bi bi-person text-muted"></i>
